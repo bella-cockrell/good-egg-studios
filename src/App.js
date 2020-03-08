55e4d49f2eb5b575619dffe39d91da875778dc51
bella-cockrell <heybellac@gmail.com>
2020-03-08 20:13:49 +0000

    Adding holding landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Spritesheet from 'react-responsive-spritesheet';
+import logo from '../src/logo-animation.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+          <Spritesheet
+            className='sprite'
+            image={logo}
+            widthFrame={74}
+            heightFrame={98}
+            steps={6}
+            fps={6}
+            autoplay={true}
+            loop={true}
+            isResponsive={true}
+          />
+        <h1>
+          Welcome to Good Egg Studios
+      </h1>
+      </div>
     </div>
   );
 }
